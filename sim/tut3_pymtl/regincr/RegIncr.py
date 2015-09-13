2145a8e6c1f8ecf0da01b35b017a693640b17d9d
chrisbreed <cbr68@cornell.edu>
2015-09-13 15:08:21 -0400

    Update RegIncr.py

--- sim/tut3_pymtl/regincr/RegIncr.py
+++ sim/tut3_pymtl/regincr/RegIncr.py
@@ -38,7 +38,7 @@ class RegIncr( Model ):
     
     @s.combinational
     def block2():
-      s.out.value = s.reg_out + 2
+      s.out.value = s.reg_out + 1
       
     def line_trace( s ):
        return "{} ({}) {}".format( s.in_, s.reg_out, s.out )
